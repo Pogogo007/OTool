5d1ba6dbd413914b2c3f72f0436118f89d7dafe4
Pogogo007 <francisd246@hotmail.com>
2020-12-16 18:42:32 -0500

    make the app dark mode

--- Windows/MainWindow.py
+++ Windows/MainWindow.py
@@ -7,9 +7,10 @@ log = logging.getLogger(__name__)
 
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (QMainWindow, QDesktopWidget, QWidget, QVBoxLayout, QStyle, QStyleFactory, QStatusBar)
+from PyQt5.QtGui import QPalette, QColor
 
 
-class MainWindow(QMainWindow):
+class MainWindow(QMainWindow, QPalette):
 
     def __init__(self, title):
         super().__init__()
@@ -44,6 +45,23 @@ class MainWindow(QMainWindow):
     def setAppStyle(app):
         if "Fusion" in [st for st in QStyleFactory.keys()]:
             app.setStyle(QStyleFactory.create("Fusion"))
+            dark_palette = QPalette()
+            dark_palette.setColor(QPalette.Window, QColor(53, 53, 53))
+            dark_palette.setColor(QPalette.WindowText, Qt.white)
+            dark_palette.setColor(QPalette.Base, QColor(25, 25, 25))
+            dark_palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
+            dark_palette.setColor(QPalette.ToolTipBase, Qt.white)
+            dark_palette.setColor(QPalette.ToolTipText, Qt.white)
+            dark_palette.setColor(QPalette.Text, Qt.white)
+            dark_palette.setColor(QPalette.Button, QColor(53, 53, 53))
+            dark_palette.setColor(QPalette.ButtonText, Qt.white)
+            dark_palette.setColor(QPalette.BrightText, Qt.red)
+            dark_palette.setColor(QPalette.Link, QColor(42, 130, 218))
+            dark_palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
+            dark_palette.setColor(QPalette.HighlightedText, Qt.black)
+
+            app.setPalette(dark_palette)
+            app.setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }")
         elif sys.platform == "win32":
             app.setStyle(QStyleFactory.create("WindowsVista"))
         elif sys.platform == "linux":
